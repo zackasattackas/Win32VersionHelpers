f579ce4f4fee53ab0a71dc8fbfe7cf0119edb315
Zack Bolin <bolinzachary@gmail.com>
2018-01-16 18:26:13 -0800

    Added console application for testing.

---
+++ VersionHelpersTester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Win32VersionHelpers;
+
+namespace VersionHelpersTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Is Windows Server? {VersionHelpers.IsWindowsServer()}");
+
+            Console.ReadKey();
+        }
+    }
+}
